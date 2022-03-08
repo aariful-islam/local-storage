8e345a5b3669ac7fecb0f21cff9e436bf16072d7
arif99050 <arifulislam99050@gmail.com>
2022-03-08 22:02:37 +0600

    work with local storage like e-commerce

---
+++ app.js
@@ -0,0 +1,65 @@
+const addItem = () => {
+    const nameField = document.getElementById('product-name')
+    const name = nameField.value;
+    if(!name){
+        return;
+    }
+    // display product in the ui from input
+    displayProduct(name);
+
+    // add cart to localStorage 
+    addproductToCart(name);
+
+    // show cart from localStorage 
+
+
+
+
+    nameField.value = '';
+}
+const displayProduct = name => {
+    const ul = document.getElementById('products');
+    const li = document.createElement('li');
+    li.innerText = name;
+    ul.appendChild(li);
+}
+const getCart = () => {
+    const cart = localStorage.getItem('cart');
+    let cartObj;
+    if (cart) {
+        cartObj = JSON.parse(cart);
+
+
+    }
+    else {
+        cartObj = {};
+    }
+    return cartObj;
+}
+const addproductToCart=name=>{
+    const cart=getCart();
+    if(cart[name]){
+        cart[name]+=1;
+    }
+    else{
+        cart[name]=1;
+    }
+    const cartStrigified=JSON.stringify(cart);
+    localStorage.setItem('cart',cartStrigified);
+}
+
+const displayLocalStorageCart=()=>{
+    const cart=getCart();
+    for(const name in cart){
+        displayProduct(name);
+    }
+}
+
+
+
+ displayLocalStorageCart();
+
+ const placeOrder=()=>{
+     document.getElementById('products').textContent=''
+    localStorage.removeItem('cart')
+ }
